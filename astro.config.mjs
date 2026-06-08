c901232cc1b26d60d79a6f6e1ba38aea28968cbf
qzomedicalcollege <kudajbergenrasul01@gmail.com>
2026-06-08 20:29:00 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
-// Для GitHub Pages project site:
-// https://<username>.github.io/qzomedicalcollege-site-v2/
-// Если позже подключишь свой домен, поменяй base на '/'.
 export default defineConfig({
-  site: 'https://example.github.io',
+  site: 'https://qzomedicalcollege.github.io',
   base: '/qzomedicalcollege-site-v2',
   trailingSlash: 'never',
   build: {
     format: 'file'
-  },
-  integrations: [sitemap()]
+  }
 });
